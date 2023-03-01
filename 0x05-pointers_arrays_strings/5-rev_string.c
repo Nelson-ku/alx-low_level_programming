a2aa229c92244426ac54c47fd1eda7d00268b550
Nelson-ku <oginganelson2018@gmail.com>
2023-03-01 07:28:59 -0800

    fix

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int x = strlen(s) - 1, y = 0;
+	char t;
+
+	for (; x > y; x--, y++)
+	{
+		t = s[x];
+		s[x] = s[y];
+		s[y] = t;
+	}
+}
