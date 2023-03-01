795f235e5537ce5249a8e67f8dd55ad3da805c52
Nelson-ku <oginganelson2018@gmail.com>
2023-03-01 07:42:39 -0800

    firstc

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*function  that prints every character in s string,
+*starting with the first character 
+*/
+void puts2(char *str)
+{
+  int x = 0;
+
+  while(str[x] = str[1]{
+      _putchar(str[x]);
+      x++;
+}
